3ae3a4db8dc937343203d29b44a2f09703e405a1
Giles Sandoval <20120630+gsandoval49@users.noreply.github.com>
2022-05-26 02:12:54 -0600

    Update main.py
    added comments

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+# Giles Sandoval
+# I play guitar and dabble in some silversmithing 
+# My baby boys are my sound engineers. Mainly they just play with the dials until we achieve the rad sound we like. 
+
 print(" GGG " + "  " + " SSS ")
 print("G   G" + "  " + "S   S")
 print("G    " + "  " + "S    ")
