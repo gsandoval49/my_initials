698349aa00337635d99eaf7713e112a863f17063
Giles Sandoval <20120630+gsandoval49@users.noreply.github.com>
2024-09-21 21:20:04 -0600

    added full name

--- main.py
+++ main.py
@@ -2,10 +2,8 @@
 # I play guitar and dabble in some silversmithing 
 # My baby boys are my sound engineers. Mainly they just play with the dials until we achieve the rad sound we like. 
 
-print(" GGG " + "  " + " SSS ")
-print("G   G" + "  " + "S   S")
-print("G    " + "  " + "S    ")
-print("GGGGG" + "  " + " SSS ")
-print("G   G" + "  " + "    S")
-print("G   G" + "  " + "S   S")
-print(" GGG " + "  " + " SSS ")
+print("  GGGG  III  L       EEEEE   SSSS ")
+print(" G       I   L       E      S     ")
+print(" G  GG   I   L       EEEE    SSS  ")
+print(" G   G   I   L       E          S ")
+print("  GGGG  III  LLLLL   EEEEE  SSSS  ")
